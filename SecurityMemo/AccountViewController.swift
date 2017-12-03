d184fc72bba4b7d3ae1eb143916a081d9d4a494a
FrankYanqingSun <yanqing.sun@wustl.edu>
2017-12-03 10:42:39 -0600

    add login and account view controllers and modifications on storyboard

---
+++ SecurityMemo/AccountViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AccountViewController.swift
+//  SecurityMemo
+//
+//  Created by Angelica Tao on 12/3/17.
+//  Copyright © 2017 SecurityMemoTeam. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    
+    var username:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
